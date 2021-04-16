211786e62d6296825c20027d724862912ed46214
Diana <dianacrainic801@yahoo.com>
2021-04-16 13:48:41 +0300

    feat: users controller tests

--- HotDiggetyDogTests/UsersControllerTests.cs
+++ HotDiggetyDogTests/UsersControllerTests.cs
@@ -1,8 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using WebAPI.Controllers;
 using WebAPI.Data.Repository.v1;
+using WebAPI.Dtos.Account;
 using WebAPI.Entities;
 using WebAPI.Helpers;
 using WebAPI.Services;
@@ -35,5 +39,78 @@ namespace HotDiggetyDogTests
             // Assert
             Assert.IsType<OkObjectResult>(actionResult.Result);
         }
+
+
+        [Fact]
+        public async void GetCustomers_ShouldReturn_OK()
+        {
+            // Act
+            ActionResult<IEnumerable<User>> actionResult = await _usersController.GetCustomers();
+
+            // Assert
+            Assert.IsType<OkObjectResult>(actionResult.Result);
+        }
+
+        [Fact]
+        public async void GetUserBy_Generated_Id_ShouldReturn_NotFound()
+        {
+            // Arrange
+            Guid id = Guid.NewGuid();
+
+            // Act
+            ActionResult<User> actionResult = await _usersController.GetUserById(id);
+
+            // Assert
+            Assert.IsType<NotFoundObjectResult>(actionResult.Result);
+        }
+
+        [Fact]
+        public async void RegisterRequestFor_New_User_ShouldReturn_CreatedAtAction()
+        {
+            // Arrange
+            RegisterRequest registerRequest = new RegisterRequest
+            {
+                Username = "admin2",
+                Email = "admin2@gmail.com",
+                Password = "admin2"
+            };
+
+            // Act
+            ActionResult<User> actionResult = await _usersController.Register(registerRequest);
+
+            // Assert
+            Assert.IsType<CreatedAtActionResult>(actionResult.Result);
+        }
+
+        [Fact]
+        public async void AuthenticateRequestFor_NonExisting_User_ShouldReturn_BadRequest()
+        {
+            // Arrange
+            AuthenticateRequest authenticateRequest = new AuthenticateRequest
+            {
+                Username = "administrator",
+                Password = "administrator"
+            };
+
+            // Act
+            ActionResult<User> actionResult = await _usersController.Authenticate(authenticateRequest);
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(actionResult.Result);
+        }
+
+        [Fact]
+        public async void Delete_NonExisting_User_ShouldReturn_NotFound()
+        {
+            // Arrange
+            Guid id = Guid.NewGuid();
+
+            // Act
+            ActionResult<User> actionResult = await _usersController.DeleteUser(id);
+
+            // Assert
+            Assert.IsType<NotFoundObjectResult>(actionResult.Result);
+        }
+
     }
 }
